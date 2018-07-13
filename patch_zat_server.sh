6d9a7d07b54b3865ee52fe7513c7c37da1bf9c35
Yuhui Xiong <yuhui.xiong.2018@gmail.com>
2018-07-13 11:31:06 +0200

    update patch file for zendesk_apps_tools-2.9.5

--- patch_zat_server.sh
+++ patch_zat_server.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-
+# valid for 2.9.5
 cd /usr/local/bundle/gems/zendesk_apps_tools-*/lib/zendesk_apps_tools/ && cat  << EOF | patch server.rb
 --- server.rb.original
 +++ server.rb.modified
-@@ -5,6 +5,7 @@
-   class Server < Sinatra::Base
-     set :protection, :except => :frame_options
-     last_mtime = Time.new(0)
+@@ -8,6 +8,7 @@
+     set :server, :thin
+     set :logging, true
+     set :protection, except: :frame_options
 +    set :bind, "0.0.0.0"
-     ZENDESK_DOMAINS_REGEX = /^http(?:s)?:\/\/[a-z0-9-]+\.(?:zendesk|zopim|zd-(?:dev|master|staging))\.com$/
+     ZENDESK_DOMAINS_REGEX = %r{^http(?:s)?://[a-z0-9-]+\.(?:zendesk|zopim|zd-(?:dev|master|staging))\.com$}
 
      get '/app.js' do
 EOF
